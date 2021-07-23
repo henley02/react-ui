5ef4dfcffe952b2406399edaae0221bdec168079
huanzhiqiang <huanzhiqiang@jiangduoduo.com>
2021-07-23 18:36:23 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, {} from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-    const a = 123;
-    if (a===123) {
+  const a = 123;
 
-        console.log(12312);
-    }
+  if (a === 123) {
+    console.log(12312);
+  }
 
-    return (
-        <div className="App">
-            <div className="app">1312312</div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="app">1312312</div>
+    </div>
+  );
 }
 
 export default App;
